889e9f5b72ae5676912f0619086fe64d41af4a53
ST10179738 <st10179738@rcconnect.edu.za>
2023-04-24 20:09:13 +0200

    first commit

---
+++ Ice_task2/Program.cs
@@ -0,0 +1,54 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string name;
+        double weight ;
+        double endWeight ;
+        double sum;
+       
+
+
+        Console.Write("Enter name <Or zzz to stop: ");
+        name = Console.ReadLine();
+       
+
+
+        while (name != "zzz") {
+            Console.Write("Start weight for {0}: ", name);
+            weight = Convert.ToDouble(Console.ReadLine());
+            Console.Write("End weight for {0}: ", name);
+            endWeight = Convert.ToDouble(Console.ReadLine());
+            sum = weight - endWeight;
+
+            if (weight > endWeight)
+            {
+                Console.WriteLine("{0}, you lost {1} kg", name, sum);
+                
+            }
+            else
+            {
+                if (weight < endWeight)
+                {
+                    Console.WriteLine("{0}, you gained {1} kg", name, sum);
+                    
+                }
+                else
+                {
+                    Console.WriteLine("{0}, you lost no weight {1} kg", name , sum);
+                    
+                }
+            }
+           
+
+
+            Console.Write("Enter name <Or zzz to stop: ");
+            name = Console.ReadLine();
+           
+        }
+        Console.WriteLine("1 lost weight");
+        Console.WriteLine("1 gained weight");
+        Console.WriteLine("1 stayed the same");
+
+    }
+}
